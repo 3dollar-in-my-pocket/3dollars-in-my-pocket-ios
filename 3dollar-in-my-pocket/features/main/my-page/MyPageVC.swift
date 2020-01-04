bc1ce1828d973b874b73cb9693cbf0b317800fb0
Yoo Hyun Sik <dbgustlr92@gmail.com>
2020-01-04 16:19:05 +0900

    [마이 페이지] 등록한 음식점까지 그림

--- 3dollar-in-my-pocket/features/main/my-page/MyPageVC.swift
+++ 3dollar-in-my-pocket/features/main/my-page/MyPageVC.swift
@@ -12,9 +12,30 @@ class MyPageVC: BaseVC {
     override func viewDidLoad() {
         super.viewDidLoad()
         view = myPageView
+        myPageView.registerCollectionView.delegate = self
+        myPageView.registerCollectionView.dataSource = self
+        myPageView.registerCollectionView.register(RegisterCell.self, forCellWithReuseIdentifier: RegisterCell.registerId)
     }
     
     override func bindViewModel() {
         
+    }
+}
+
+extension MyPageVC: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: RegisterCell.registerId, for: indexPath) as? RegisterCell else {
+            return BaseCollectionViewCell()
+        }
+        
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: 172, height: 172)
     }
 }
---
+++ 3dollar-in-my-pocket/features/main/my-page/RegisterCell.swift
@@ -0,0 +1,73 @@
+import UIKit
+
+class RegisterCell: BaseCollectionViewCell {
+    
+    static let registerId = "\(RegisterCell.self)"
+    
+    let categoryImage = UIImageView().then {
+        $0.image = UIImage.init(named: "img_my_page_fish")
+    }
+    
+    let titleLabel = UILabel().then {
+        $0.text = "강남역 2번출구 앞"
+        $0.textColor = .white
+        $0.font = UIFont.init(name: "SpoqaHanSans-Bold", size: 16)
+    }
+    
+    let rankingView = RankingView()
+    
+    
+    override func setup() {
+        backgroundColor = UIColor.init(r: 74, g: 74, b: 74)
+        layer.cornerRadius = 16
+        addSubViews(categoryImage, titleLabel, rankingView)
+        setupShadow()
+    }
+    
+    override func bindConstraints() {
+        categoryImage.snp.makeConstraints { (make) in
+            make.centerX.equalToSuperview()
+            make.top.equalToSuperview().offset(24)
+            make.width.equalTo(115)
+            make.height.equalTo(73)
+        }
+        
+        titleLabel.snp.makeConstraints { (make) in
+            make.left.equalToSuperview().offset(16)
+            make.top.equalTo(categoryImage.snp.bottom).offset(11)
+        }
+        
+        rankingView.snp.makeConstraints { (make) in
+            make.left.equalToSuperview().offset(16)
+            make.right.equalToSuperview().offset(-14)
+            make.bottom.equalToSuperview().offset(-18)
+            make.height.equalTo(20)
+        }
+    }
+    
+    func setSelected(isSelected: Bool) {
+        if let shadowLayer = layer.sublayers?.first as? CAShapeLayer {
+            if isSelected {
+                shadowLayer.fillColor = UIColor.init(r: 28, g: 28, b: 28).cgColor
+            } else {
+                shadowLayer.fillColor = UIColor.init(r: 251, g: 251, b: 251).cgColor
+            }
+        }
+    }
+    
+    private func setupShadow() {
+        let shadowLayer = CAShapeLayer()
+        
+        shadowLayer.path = UIBezierPath(roundedRect: bounds, cornerRadius: 16).cgPath
+        shadowLayer.fillColor = UIColor.init(r: 74, g: 74, b: 74).cgColor
+        shadowLayer.masksToBounds = false
+        shadowLayer.shouldRasterize = true
+        shadowLayer.shadowColor = UIColor.black.cgColor
+        shadowLayer.shadowPath = nil
+        shadowLayer.shadowOffset = CGSize(width: 5, height: 10)
+        shadowLayer.shadowOpacity = 0.24
+        shadowLayer.shadowRadius = 5
+        
+        layer.insertSublayer(shadowLayer, at: 0)
+    }
+}
