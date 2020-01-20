01471ff5fb118afcfa6e8439a525d53f75200cf5
Yoo Hyun Sik <dbgustlr92@gmail.com>
2020-01-20 10:38:40 +0900

    리뷰 저장 API query, body 같이 사용하도록 수정

--- 3dollar-in-my-pocket/network/ReviewService.swift
+++ 3dollar-in-my-pocket/network/ReviewService.swift
@@ -3,14 +3,9 @@ import ObjectMapper
 
 struct ReviewService: APIServiceType {
     static func saveReview(review: Review, storeId: Int, completion: @escaping ((DataResponse<String>) -> Void)) {
-        let urlString = self.url("api/v1/review/save")
+        let urlString = self.url("api/v1/review/save?storeId=\(storeId)&userId=\(UserDefaultsUtil.getUserId()!)")
         let headers = self.jsonWithTokenHeader()
-        var parameter = review.toJSON()
-        
-        parameter["storeId"] = storeId
-        if let userId = UserDefaultsUtil.getUserId() {
-            parameter["userId"] = userId
-        }
+        let parameter = review.toJSON()
         
         Alamofire.request(urlString, method: .post, parameters: parameter, encoding: JSONEncoding.default, headers: headers).responseString { (response) in
             completion(response)
