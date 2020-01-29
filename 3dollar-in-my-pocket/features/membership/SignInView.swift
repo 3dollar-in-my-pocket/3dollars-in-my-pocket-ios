43e1503c0e7df2d27a985d6ed0bd6a3b7bcd0289
Yoo Hyun Sik <dbgustlr92@gmail.com>
2020-01-29 13:33:18 +0900

    로그인화면 디자인 가이드 적용

--- 3dollar-in-my-pocket/features/membership/SignInView.swift
+++ 3dollar-in-my-pocket/features/membership/SignInView.swift
@@ -14,36 +14,60 @@ class SignInView: BaseView {
         $0.play()
     }
     
-    let kakaoBtn = KOLoginButton()
+    let kakaoBtn = UIButton().then {
+        $0.layer.cornerRadius = 20
+        $0.backgroundColor = UIColor.init(r: 247, g: 227, b: 23)
+    }
     
-    let appleBtn = ASAuthorizationAppleIDButton()
+    let kakaoLabel = UILabel().then {
+        $0.text = "카카오 계정으로 로그인"
+        $0.font = UIFont.init(name: "SpoqaHanSans-Bold", size: 14)
+        $0.textColor = UIColor.init(r: 56, g: 30, b: 31)
+    }
+    
+    let kakaoImg = UIImageView().then {
+        $0.image = UIImage.init(named: "ic_kakao")
+    }
+    
+    let appleBtn = ASAuthorizationAppleIDButton(type: .signIn, style: .white).then {
+        $0.cornerRadius = 24
+    }
     
     
     override func setup() {
         alpha = 0
         backgroundColor = UIColor.init(r: 28, g: 28, b: 28)
-        addSubViews(lottie, kakaoBtn, appleBtn)
+        addSubViews(lottie, kakaoBtn, kakaoImg, kakaoLabel, appleBtn)
     }
     
     override func bindConstraints() {
-        
         lottie.snp.makeConstraints { (make) in
             make.left.right.equalToSuperview()
-            make.top.equalToSuperview().offset(70)
+            make.top.equalToSuperview().offset(109)
             make.height.equalTo(350)
         }
         
         appleBtn.snp.makeConstraints { (make) in
-            make.left.equalToSuperview().offset(20)
-            make.right.equalToSuperview().offset(-20)
-            make.bottom.equalToSuperview().offset(-100)
-            make.height.equalTo(48)
+            make.left.right.equalTo(kakaoBtn)
+            make.top.equalTo(kakaoBtn.snp.bottom).offset(16)
+            make.height.equalTo(40)
         }
         
         kakaoBtn.snp.makeConstraints { (make) in
-            make.left.right.equalTo(appleBtn)
-            make.height.equalTo(48)
-            make.bottom.equalTo(appleBtn.snp.top).offset(-20)
+            make.left.equalToSuperview().offset(31)
+            make.right.equalToSuperview().offset(-32)
+            make.top.equalTo(lottie.snp.bottom).offset(30)
+            make.height.equalTo(40)
+        }
+        
+        kakaoLabel.snp.makeConstraints { (make) in
+            make.center.equalTo(kakaoBtn)
+        }
+        
+        kakaoImg.snp.makeConstraints { (make) in
+            make.centerY.equalTo(kakaoLabel)
+            make.right.equalTo(kakaoLabel.snp.left).offset(-6)
+            make.width.height.equalTo(16)
         }
     }
     
