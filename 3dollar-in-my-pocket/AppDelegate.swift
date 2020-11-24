4cbb391c4e8faa293bc39458dd09a8978f820841
Yoo Hyun Sik <dbgustlr92@gmail.com>
2020-11-24 23:42:02 +0900

    네이버 지도 pod 추가

--- 3dollar-in-my-pocket/AppDelegate.swift
+++ 3dollar-in-my-pocket/AppDelegate.swift
@@ -7,61 +7,61 @@ import GoogleMobileAds
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
-        initializeFirebase()
-        initializeGoogleMaps()
-        initializeNetworkLogger()
-      
-        return true
-    }
-
-    // MARK: UISceneSession Lifecycle
-
-    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-        // Called when a new scene session is being created.
-        // Use this method to select a configuration to create the new scene with.
-        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
-    }
-
-    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
-        // Called when the user discards a scene session.
-        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
-        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+  
+  func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+    // Override point for customization after application launch.
+    initializeFirebase()
+    initializeGoogleMaps()
+    initializeNetworkLogger()
+    
+    return true
+  }
+  
+  // MARK: UISceneSession Lifecycle
+  
+  func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+    // Called when a new scene session is being created.
+    // Use this method to select a configuration to create the new scene with.
+    return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+  }
+  
+  func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
+    // Called when the user discards a scene session.
+    // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
+    // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+  }
+  
+  func application(_ application: UIApplication, open url: URL, sourceApplication: String?, annotation: Any) -> Bool {
+    if KOSession.isKakaoAccountLoginCallback(url) {
+      return KOSession.handleOpen(url)
     }
     
-    func application(_ application: UIApplication, open url: URL, sourceApplication: String?, annotation: Any) -> Bool {
-        if KOSession.isKakaoAccountLoginCallback(url) {
-            return KOSession.handleOpen(url)
-        }
-        
-        return false
+    return false
+  }
+  
+  func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
+    if KOSession.isKakaoAccountLoginCallback(url) {
+      return KOSession.handleOpen(url)
     }
-    
-    func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
-        if KOSession.isKakaoAccountLoginCallback(url) {
-            return KOSession.handleOpen(url)
-        }
-        return false
-    }
-    
-    private func initializeGoogleMaps() {
-        GMSServices.provideAPIKey("AIzaSyA7CMJwuD1RrlhgF-L1o9vuKOgFiNA-5Sg")
-    }
-    
-    private func initializeNetworkLogger() {
-        NetworkActivityLogger.shared.startLogging()
-        NetworkActivityLogger.shared.level = .debug
-    }
-    
-    private func initializeFirebase() {
-        FirebaseApp.configure()
-    }
-    
-    private func initilizeAdmob() {
-        GADMobileAds.sharedInstance().start(completionHandler: nil)
-    }
+    return false
+  }
+  
+  private func initializeGoogleMaps() {
+    GMSServices.provideAPIKey("AIzaSyA7CMJwuD1RrlhgF-L1o9vuKOgFiNA-5Sg")
+  }
+  
+  private func initializeNetworkLogger() {
+    NetworkActivityLogger.shared.startLogging()
+    NetworkActivityLogger.shared.level = .debug
+  }
+  
+  private func initializeFirebase() {
+    FirebaseApp.configure()
+  }
+  
+  private func initilizeAdmob() {
+    GADMobileAds.sharedInstance().start(completionHandler: nil)
+  }
 }
 
 
