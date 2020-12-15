037d24858086a4484265b270969793682e92b3a7
Yoo Hyun Sik <dbgustlr92@gmail.com>
2020-12-15 22:46:26 +0900

    카테고리 화면 API여러번 호출하는 오류 수정

--- 3dollar-in-my-pocket/features/main/home/category-list/CategoryChildVC.swift
+++ 3dollar-in-my-pocket/features/main/home/category-list/CategoryChildVC.swift
@@ -1,202 +1,198 @@
 import UIKit
 
 protocol CategoryChildDelegate {
-    func setMarkers(storeCards: [StoreCard])
+  func setMarkers(storeCards: [StoreCard])
 }
 
 class CategoryChildVC: BaseVC {
-    
-    var viewModel = CategoryChildViewModel()
-    var delegate: CategoryChildDelegate?
-    var category: StoreCategory!
-    var latitude: Double!
-    var longitude: Double!
-    var order = Order.DISTANCE
-    
-    private lazy var categoryChildView = CategoryChildView.init(category: self.category)
-    
-    static func instance(category: StoreCategory, latitude: Double, longitude: Double) -> CategoryChildVC {
-        return CategoryChildVC.init(nibName: nil, bundle: nil).then {
-            $0.category = category
-            $0.latitude = latitude
-            $0.longitude = longitude
-        }
+  
+  var viewModel = CategoryChildViewModel()
+  var delegate: CategoryChildDelegate?
+  var category: StoreCategory!
+  var latitude: Double!
+  var longitude: Double!
+  var order = Order.DISTANCE
+  
+  private lazy var categoryChildView = CategoryChildView.init(category: self.category)
+  
+  static func instance(category: StoreCategory, latitude: Double, longitude: Double) -> CategoryChildVC {
+    return CategoryChildVC.init(nibName: nil, bundle: nil).then {
+      $0.category = category
+      $0.latitude = latitude
+      $0.longitude = longitude
     }
+  }
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    view = categoryChildView
+    setupTableView()
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        view = categoryChildView
-        setupTableView()
+    if categoryChildView.nearOrderBtn.isSelected {
+      getStoreByDistance()
+    } else {
+      getStoreByReview()
     }
+  }
+  
+  override func bindViewModel() {
+    categoryChildView.nearOrderBtn.rx.tap.bind { [weak self] (_) in
+      if let vc = self,
+         !vc.categoryChildView.nearOrderBtn.isSelected {
+        vc.order = .DISTANCE
+        vc.categoryChildView.nearOrderBtn.isSelected = true
+        vc.categoryChildView.reviewOrderBtn.isSelected = false
+        vc.getStoreByDistance()
+      }
+    }.disposed(by: disposeBag)
     
-    override func bindViewModel() {
-        categoryChildView.nearOrderBtn.rx.tap.bind { [weak self] (_) in
-            if let vc = self,
-                !vc.categoryChildView.nearOrderBtn.isSelected {
-                vc.order = .DISTANCE
-                vc.categoryChildView.nearOrderBtn.isSelected = true
-                vc.categoryChildView.reviewOrderBtn.isSelected = false
-                vc.getStoreByDistance()
-            }
-        }.disposed(by: disposeBag)
-        
-        categoryChildView.reviewOrderBtn.rx.tap.bind { [weak self] (_) in
-            if let vc = self,
-                !vc.categoryChildView.reviewOrderBtn.isSelected {
-                vc.order = .REVIEW
-                vc.categoryChildView.nearOrderBtn.isSelected = false
-                vc.categoryChildView.reviewOrderBtn.isSelected = true
-                vc.getStoreByReview()
-            }
-        }.disposed(by: disposeBag)
-    }
-    
-    override func viewDidAppear(_ animated: Bool) {
-        super.viewDidAppear(animated)
-        
-        if categoryChildView.nearOrderBtn.isSelected {
-            getStoreByDistance()
-        } else {
-            getStoreByReview()
-        }
-    }
-    
-    private func setupTableView() {
-        categoryChildView.tableView.delegate = self
-        categoryChildView.tableView.dataSource = self
-        categoryChildView.tableView.register(CategoryListCell.self, forCellReuseIdentifier: CategoryListCell.registerId)
-    }
-    
-    private func getStoreByDistance() {
-        CategoryService.getStroeByDistance(
-          category: category,
-          latitude: latitude,
-          longitude: longitude
-        ).subscribe(onNext: { [weak self] categoryByDistance in
-          guard let self = self else { return }
-          
-          self.viewModel.setDistance(storeByDistance: categoryByDistance)
-          self.categoryChildView.setEmpty(isEmpty: self.viewModel.isDistanceEmpty())
-          self.delegate?.setMarkers(storeCards: self.viewModel.getAllDistanceStores())
-          self.categoryChildView.tableView.reloadData()
-        }, onError: { [weak self] error in
-          guard let self = self else { return }
-          
-          AlertUtils.show(
-            controller: self,
-            title: "get store by distance",
-            message: error.localizedDescription
-          )
-        })
-        .disposed(by: disposeBag)
-    }
-    
-    private func getStoreByReview() {
-        CategoryService.getStoreByReview(
-          category: category,
-          latitude: latitude,
-          longitude: longitude
-        ).subscribe(onNext: { [weak self] categoryByReview in
-          guard let self = self else { return }
-          
-          self.viewModel.setReview(storeByReview: categoryByReview)
-          self.categoryChildView.setEmpty(isEmpty: self.viewModel.isReviewEmpty())
-          self.delegate?.setMarkers(storeCards: self.viewModel.getAllReviewStores())
-          self.categoryChildView.tableView.reloadData()
-        }, onError: { [weak self] error in
-          guard let self = self else { return }
-          
-          AlertUtils.show(
-            controller: self,
-            title: "get store by review",
-            message: error.localizedDescription
-          )
-        }).disposed(by: disposeBag)
-    }
+    categoryChildView.reviewOrderBtn.rx.tap.bind { [weak self] (_) in
+      if let vc = self,
+         !vc.categoryChildView.reviewOrderBtn.isSelected {
+        vc.order = .REVIEW
+        vc.categoryChildView.nearOrderBtn.isSelected = false
+        vc.categoryChildView.reviewOrderBtn.isSelected = true
+        vc.getStoreByReview()
+      }
+    }.disposed(by: disposeBag)
+  }
+  
+  private func setupTableView() {
+    categoryChildView.tableView.delegate = self
+    categoryChildView.tableView.dataSource = self
+    categoryChildView.tableView.register(CategoryListCell.self, forCellReuseIdentifier: CategoryListCell.registerId)
+  }
+  
+  private func getStoreByDistance() {
+    CategoryService.getStroeByDistance(
+      category: category,
+      latitude: latitude,
+      longitude: longitude
+    ).subscribe(onNext: { [weak self] categoryByDistance in
+      guard let self = self else { return }
+      
+      self.viewModel.setDistance(storeByDistance: categoryByDistance)
+      self.categoryChildView.setEmpty(isEmpty: self.viewModel.isDistanceEmpty())
+      self.delegate?.setMarkers(storeCards: self.viewModel.getAllDistanceStores())
+      self.categoryChildView.tableView.reloadData()
+    }, onError: { [weak self] error in
+      guard let self = self else { return }
+      
+      AlertUtils.show(
+        controller: self,
+        title: "get store by distance",
+        message: error.localizedDescription
+      )
+    })
+    .disposed(by: disposeBag)
+  }
+  
+  private func getStoreByReview() {
+    CategoryService.getStoreByReview(
+      category: category,
+      latitude: latitude,
+      longitude: longitude
+    ).subscribe(onNext: { [weak self] categoryByReview in
+      guard let self = self else { return }
+      
+      self.viewModel.setReview(storeByReview: categoryByReview)
+      self.categoryChildView.setEmpty(isEmpty: self.viewModel.isReviewEmpty())
+      self.delegate?.setMarkers(storeCards: self.viewModel.getAllReviewStores())
+      self.categoryChildView.tableView.reloadData()
+    }, onError: { [weak self] error in
+      guard let self = self else { return }
+      
+      AlertUtils.show(
+        controller: self,
+        title: "get store by review",
+        message: error.localizedDescription
+      )
+    }).disposed(by: disposeBag)
+  }
 }
 
 extension CategoryChildVC: UITableViewDelegate, UITableViewDataSource {
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        switch self.order {
-        case .DISTANCE:
-            return self.viewModel.getNumberOfDistanceRow(section: section)
-        case .REVIEW:
-            return self.viewModel.getNumberOfReviewRow(section: section)
-        }
+  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    switch self.order {
+    case .DISTANCE:
+      return self.viewModel.getNumberOfDistanceRow(section: section)
+    case .REVIEW:
+      return self.viewModel.getNumberOfReviewRow(section: section)
+    }
+  }
+  
+  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    guard let cell = tableView.dequeueReusableCell(withIdentifier: CategoryListCell.registerId, for: indexPath) as? CategoryListCell else {
+      return BaseTableViewCell()
     }
     
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        guard let cell = tableView.dequeueReusableCell(withIdentifier: CategoryListCell.registerId, for: indexPath) as? CategoryListCell else {
-            return BaseTableViewCell()
-        }
-        
-        cell.setBottomRadius(isLast: tableView.numberOfRows(inSection: indexPath.section) - 1 == indexPath.row)
-        if indexPath.row % 2 == 0 {
-            cell.setEvenBg()
-        } else {
-            cell.setOddBg()
-        }
-        
-        switch self.order {
-        case .DISTANCE:
-            if let storeCard = self.viewModel.getDistanceStore(indexPath: indexPath) {
-                cell.bind(order: .DISTANCE, storeCard: storeCard)
-            }
-        default:
-            if let storeCard = self.viewModel.getReviewStore(indexPath: indexPath) {
-                cell.bind(order: .REVIEW, storeCard: storeCard)
-            }
-        }
-        return cell
-    }
-
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        switch self.order {
-        case .DISTANCE:
-            if let storeCard = self.viewModel.getDistanceStore(indexPath: indexPath) {
-                self.navigationController?.pushViewController(DetailVC.instance(storeId: storeCard.id), animated: true)
-            }
-        case .REVIEW:
-            if let storeCard = self.viewModel.getReviewStore(indexPath: indexPath) {
-                self.navigationController?.pushViewController(DetailVC.instance(storeId: storeCard.id), animated: true)
-            }
-        }
+    cell.setBottomRadius(isLast: tableView.numberOfRows(inSection: indexPath.section) - 1 == indexPath.row)
+    if indexPath.row % 2 == 0 {
+      cell.setEvenBg()
+    } else {
+      cell.setOddBg()
     }
     
-    func numberOfSections(in tableView: UITableView) -> Int {
-        switch self.order {
-        case .DISTANCE:
-            return self.viewModel.getValidDistanceCount()
-        case .REVIEW:
-            return self.viewModel.getValidReviewCount()
-        }
+    switch self.order {
+    case .DISTANCE:
+      if let storeCard = self.viewModel.getDistanceStore(indexPath: indexPath) {
+        cell.bind(order: .DISTANCE, storeCard: storeCard)
+      }
+    default:
+      if let storeCard = self.viewModel.getReviewStore(indexPath: indexPath) {
+        cell.bind(order: .REVIEW, storeCard: storeCard)
+      }
     }
-    
-    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
-        switch self.order {
-        case .DISTANCE:
-            if self.viewModel.isValidDistanceSection(section: section) {
-                return CategoryListHeaderView().then {
-                    $0.setDistanceHeader(section: self.viewModel.storeByDistance.indexList[section])
-                }
-            } else {
-                return nil
-            }
-        case .REVIEW:
-            if self.viewModel.isValidReviewSection(section: section) {
-                return CategoryListHeaderView().then {
-                    $0.setReviewHeader(section: self.viewModel.storeByReview.indexList[section])
-                }
-            } else {
-                return nil
-            }
-        }
+    return cell
+  }
+  
+  func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+    switch self.order {
+    case .DISTANCE:
+      if let storeCard = self.viewModel.getDistanceStore(indexPath: indexPath) {
+        self.navigationController?.pushViewController(DetailVC.instance(storeId: storeCard.id), animated: true)
+      }
+    case .REVIEW:
+      if let storeCard = self.viewModel.getReviewStore(indexPath: indexPath) {
+        self.navigationController?.pushViewController(DetailVC.instance(storeId: storeCard.id), animated: true)
+      }
     }
+  }
+  
+  func numberOfSections(in tableView: UITableView) -> Int {
+    switch self.order {
+    case .DISTANCE:
+      return self.viewModel.getValidDistanceCount()
+    case .REVIEW:
+      return self.viewModel.getValidReviewCount()
+    }
+  }
+  
+  func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+    switch self.order {
+    case .DISTANCE:
+      if self.viewModel.isValidDistanceSection(section: section) {
+        return CategoryListHeaderView().then {
+          $0.setDistanceHeader(section: self.viewModel.storeByDistance.indexList[section])
+        }
+      } else {
+        return nil
+      }
+    case .REVIEW:
+      if self.viewModel.isValidReviewSection(section: section) {
+        return CategoryListHeaderView().then {
+          $0.setReviewHeader(section: self.viewModel.storeByReview.indexList[section])
+        }
+      } else {
+        return nil
+      }
+    }
+  }
 }
 
 public enum Order: String {
-    case DISTANCE = "DISTANCE"
-    case REVIEW = "REVIEW"
+  case DISTANCE = "DISTANCE"
+  case REVIEW = "REVIEW"
 }
 
 
