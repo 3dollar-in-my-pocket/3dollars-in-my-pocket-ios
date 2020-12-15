37a55815843a3b23396d159695a7b06e50009e6c
Yoo Hyun Sik <dbgustlr92@gmail.com>
2020-12-15 23:08:17 +0900

    카카오톡 공유하기 URL 서버 이미지 적용

--- 3dollar-in-my-pocket/features/main/home/detail/DetailViewModel.swift
+++ 3dollar-in-my-pocket/features/main/home/detail/DetailViewModel.swift
@@ -45,7 +45,7 @@ class DetailViewModel: BaseViewModel {
     )
     let content = Content(
       title: "올 때 붕어빵, 잊지마!!!",
-      imageUrl: URL(string: "https://firebasestorage.googleapis.com/v0/b/dollar-in-my-pocket.appspot.com/o/kakao_link.png?alt=media&token=c3dd508f-63c6-4645-bff6-808c5a902417")!,
+      imageUrl: URL(string: HTTPUtils.url + "/images/share-with-kakao.png")!,
       imageWidth: 500,
       imageHeight: 500,
       description: "당신은 붕어빵 셔틀에 당첨되셨습니다.\n지금 바로 가슴속 3천원을 설치하고 위치를 파악하여 미션을 수행하세요!",
