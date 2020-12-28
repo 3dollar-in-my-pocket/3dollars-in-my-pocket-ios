d3744e91c29cda6413e5ed628f7baf97fa02d578
hyunsik yoo <hyunsikyoo@seerslab.com>
2020-12-28 14:04:46 +0900

    GA 이벤트 정의중

--- 3dollar-in-my-pocket/manager/GAEvent.swift
+++ 3dollar-in-my-pocket/manager/GAEvent.swift
@@ -1,3 +1,24 @@
 enum GAEvent: String {
-  case TEST = "test"
+  // Login_page
+  case login_kakao_login_button_clicked = "login_kakao_login_button_clicked"
+  case login_apple_login_button_clicked = "login_apple_login_button_clicked"
+  
+  // Nickname_initilize_page
+  case nickname_initilize_back_button_clicked = "nickname_initilize_back_button_clicked"
+  case nickname_initilize_change_button_clicked = "nickname_initilize_change_button_clicked"
+  case nickname_initilize_already_existed = "nickname_initilize_already_existed"
+  
+  // Nickname_change_page
+  case nickname_change_back_button_clicked = "nickname_change_back_button_clicked"
+  case nickname_change_change_button_clicked = "nickname_change_change_button_clicked"
+  case back_button_clicked = "back_button_clicked"
+  case nickname_change_already_existed = "nickname_change_already_existed"
+  
+  // Home_page
+  case home_filter_bungeoppang_button_clicked = "home_filter_bungeoppang_button_clicked"
+  case home_filter_takoyaki_button_clicked = "home_filter_takoyaki_button_clicked"
+  case home_filter_gyeranppang_button_clicked = "home_filter_gaeranppang_button_clicked"
+  case home_filter_hotteok_button_clicked = "home_filter_hotteok_button_clicked"
+  case home_store_card_button_clicked = "home_store_card_button_clicked"
+  case home_store_card_swiped = "home_store_card_swiped"
 }
