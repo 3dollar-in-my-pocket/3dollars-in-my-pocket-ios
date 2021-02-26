fe0b0834ebf5adccde625b92df18fb11d4fa6e43
hyunsik yoo <hyunsikyoo@seerslab.com>
2021-02-26 12:55:52 +0900

    feat: My-Page MVVM적용

--- 3dollar-in-my-pocket/features/my-page/MyPageView.swift
+++ 3dollar-in-my-pocket/features/my-page/MyPageView.swift
@@ -8,16 +8,16 @@ class MyPageView: BaseView {
     $0.backgroundColor = UIColor.init(r: 28, g: 28, b: 28)
   }
   
-  let nicknameBg = UIView().then {
-    $0.layer.borderColor = UIColor.init(r: 243, g: 162, b: 169).cgColor
+  let nicknameContainer = UIView().then {
+    $0.layer.borderColor = UIColor.init(r: 255, g: 161, b: 170).cgColor
     $0.layer.borderWidth = 2
-    $0.layer.cornerRadius = 22
+    $0.layer.cornerRadius = 23
   }
   
   let nicknameLabel = UILabel().then {
-    $0.font = UIFont.init(name: "SpoqaHanSans-Bold", size: 16)
+    $0.font = UIFont(name: "AppleSDGothicNeoEB00", size: 16)
     $0.textAlignment = .left
-    $0.textColor = UIColor.init(r: 243, g: 162, b: 169)
+    $0.textColor = UIColor.init(r: 255, g: 161, b: 170)
   }
   
   let settingButton = UIButton().then {
@@ -29,34 +29,38 @@ class MyPageView: BaseView {
   }
   
   let registerLabel = UILabel().then {
-    $0.text = "등록한 음식점"
+    $0.text = "my_page_registered_store".localized
     $0.textColor = .white
-    $0.font = UIFont.init(name: "SpoqaHanSans-Light", size: 24)
+    $0.font = UIFont(name: "AppleSDGothicNeo-Light", size: 24)
   }
   
   let registerCountLabel = UILabel().then {
     $0.textColor = .white
-    $0.font = UIFont.init(name: "SpoqaHanSans-Bold", size: 24)
+    $0.font = UIFont(name: "AppleSDGothicNeoEB00", size: 24)
   }
   
-  let registerTotalBtn = UIButton().then {
-    $0.setTitle("전체보기", for: .normal)
-    $0.setTitleColor(UIColor.init(r: 238, g: 98, b: 76), for: .normal)
-    $0.titleLabel?.font = UIFont.init(name: "SpoqaHanSans-Regular", size: 14)
+  let registerTotalButton = UIButton().then {
+    $0.setTitle("my_page_total".localized, for: .normal)
+    $0.setTitleColor(UIColor.init(r: 255, g: 92, b: 67), for: .normal)
+    $0.titleLabel?.font = UIFont(name: "AppleSDGothicNeo-Medium", size: 14)
   }
   
-  let registerCollectionView = UICollectionView(frame: .zero, collectionViewLayout: UICollectionViewFlowLayout()).then {
+  let registerCollectionView = UICollectionView(
+    frame: .zero,
+    collectionViewLayout: UICollectionViewFlowLayout()
+  ).then {
     let layout = UICollectionViewFlowLayout()
     
     layout.scrollDirection = .horizontal
     layout.minimumLineSpacing = 16
+    layout.itemSize = CGSize(width: 172, height: 172)
     $0.collectionViewLayout = layout
     $0.showsHorizontalScrollIndicator = false
     $0.backgroundColor = .clear
     $0.contentInset = UIEdgeInsets(top: 0, left: 24, bottom: 20, right: 196)
   }
   
-  let registerEmptyImg = UIImageView().then {
+  let registerEmptyImage = UIImageView().then {
     $0.image = UIImage.init(named: "img_my_page_empty")
     $0.isHidden = true
   }
@@ -68,20 +72,20 @@ class MyPageView: BaseView {
   }
   
   let reviewLabel = UILabel().then {
-    $0.text = "내가 쓴 리뷰"
+    $0.text = "my_page_registered_review".localized
     $0.textColor = .white
-    $0.font = UIFont.init(name: "SpoqaHanSans-Light", size: 24)
+    $0.font = UIFont(name: "AppleSDGothicNeo-Light", size: 24)
   }
   
   let reviewCountLabel = UILabel().then {
     $0.textColor = .white
-    $0.font = UIFont.init(name: "SpoqaHanSans-Bold", size: 24)
+    $0.font = UIFont(name: "AppleSDGothicNeoEB00", size: 24)
   }
   
-  let reviewTotalBtn = UIButton().then {
-    $0.setTitle("전체보기", for: .normal)
-    $0.setTitleColor(UIColor.init(r: 238, g: 98, b: 76), for: .normal)
-    $0.titleLabel?.font = UIFont.init(name: "SpoqaHanSans-Regular", size: 14)
+  let reviewTotalButton = UIButton().then {
+    $0.setTitle("my_page_total".localized, for: .normal)
+    $0.setTitleColor(UIColor.init(r: 255, g: 92, b: 67), for: .normal)
+    $0.titleLabel?.font = UIFont(name: "AppleSDGothicNeo-Medium", size: 14)
   }
   
   let reviewTableView = UITableView().then {
@@ -93,116 +97,117 @@ class MyPageView: BaseView {
     $0.isScrollEnabled = false
   }
   
-  let reviewEmptyImg = UIImageView().then {
+  let reviewEmptyImage = UIImageView().then {
     $0.image = UIImage.init(named: "img_my_page_empty")
     $0.isHidden = true
   }
   
   
   override func setup() {
-    backgroundColor = UIColor.init(r: 28, g: 28, b: 28)
-    containerView.addSubViews(
-      bgCloud, nicknameBg, nicknameLabel, settingButton, registerLabel,
-      registerCountLabel, registerTotalBtn, registerCollectionView, registerEmptyBg, registerEmptyImg,
-      reviewLabel, reviewCountLabel, reviewTotalBtn, reviewTableView, reviewEmptyImg
+    self.backgroundColor = UIColor.init(r: 28, g: 28, b: 28)
+    self.containerView.addSubViews(
+      bgCloud, nicknameContainer, nicknameLabel, settingButton,
+      registerLabel, registerCountLabel, registerTotalButton,
+      registerCollectionView, registerEmptyBg, registerEmptyImage,
+      reviewLabel, reviewCountLabel, reviewTotalButton, reviewTableView,
+      reviewEmptyImage
     )
-    scrollView.addSubview(containerView)
-    addSubViews(scrollView)
+    self.scrollView.addSubview(containerView)
+    self.addSubViews(scrollView)
   }
   
   override func bindConstraints() {
-    scrollView.snp.makeConstraints { (make) in
-      make.top.equalTo(safeAreaLayoutGuide)
-      make.left.right.bottom.equalToSuperview()
+    self.scrollView.snp.makeConstraints { make in
+      make.left.top.right.equalToSuperview()
+      make.bottom.equalTo(safeAreaLayoutGuide)
     }
     
-    nicknameBg.snp.makeConstraints { (make) in
-      make.top.equalTo(safeAreaLayoutGuide).offset(35)
+    self.nicknameContainer.snp.makeConstraints { make in
+      make.top.equalToSuperview().offset(safeAreaInsets.top + 35)
       make.left.equalToSuperview().offset(24)
       make.right.equalToSuperview().offset(-24)
-      make.height.equalTo(44)
+      make.height.equalTo(46)
     }
     
-    settingButton.snp.makeConstraints { (make) in
-      make.centerY.equalTo(nicknameBg.snp.centerY)
-      make.right.equalTo(nicknameBg.snp.right).offset(-16)
+    self.settingButton.snp.makeConstraints { (make) in
+      make.centerY.equalTo(self.nicknameContainer)
+      make.right.equalTo(self.nicknameContainer).offset(-16)
       make.width.height.equalTo(24)
     }
     
-    nicknameLabel.snp.makeConstraints { (make) in
-      make.left.equalTo(nicknameBg.snp.left).offset(16)
-      make.top.equalTo(nicknameBg.snp.top).offset(11)
-      make.bottom.equalTo(nicknameBg.snp.bottom).offset(-11)
+    self.nicknameLabel.snp.makeConstraints { (make) in
+      make.centerY.equalTo(self.nicknameContainer)
+      make.left.equalTo(nicknameContainer).offset(16)
       make.right.equalTo(settingButton.snp.right).offset(-16)
     }
     
-    bgCloud.snp.makeConstraints { (make) in
+    self.bgCloud.snp.makeConstraints { (make) in
       make.left.right.equalToSuperview()
-      make.top.equalTo(nicknameBg.snp.bottom).offset(19)
+      make.top.equalTo(self.nicknameContainer.snp.bottom).offset(19)
       make.height.equalTo(135)
     }
     
-    registerLabel.snp.makeConstraints { (make) in
+    self.registerLabel.snp.makeConstraints { (make) in
       make.left.equalToSuperview().offset(24)
-      make.bottom.equalTo(bgCloud.snp.bottom)
+      make.bottom.equalTo(self.bgCloud.snp.bottom)
     }
     
-    registerCountLabel.snp.makeConstraints { (make) in
-      make.left.equalTo(registerLabel.snp.right).offset(5)
-      make.centerY.equalTo(registerLabel)
+    self.registerCountLabel.snp.makeConstraints { (make) in
+      make.left.equalTo(self.registerLabel.snp.right).offset(5)
+      make.centerY.equalTo(self.registerLabel)
     }
     
-    registerTotalBtn.snp.makeConstraints { (make) in
-      make.centerY.equalTo(registerLabel.snp.centerY)
+    self.registerTotalButton.snp.makeConstraints { (make) in
+      make.centerY.equalTo(self.registerLabel.snp.centerY)
       make.right.equalToSuperview().offset(-24)
     }
     
-    registerCollectionView.snp.makeConstraints { (make) in
+    self.registerCollectionView.snp.makeConstraints { (make) in
       make.left.right.equalToSuperview()
-      make.top.equalTo(registerLabel.snp.bottom).offset(16)
+      make.top.equalTo(self.registerLabel.snp.bottom).offset(16)
       make.height.equalTo(200)
     }
     
-    registerEmptyBg.snp.makeConstraints { (make) in
+    self.registerEmptyBg.snp.makeConstraints { (make) in
       make.width.height.equalTo(172)
       make.left.equalToSuperview().offset(20)
-      make.top.equalTo(registerLabel.snp.bottom).offset(12)
+      make.top.equalTo(self.registerLabel.snp.bottom).offset(12)
     }
     
-    registerEmptyImg.snp.makeConstraints { (make) in
-      make.center.equalTo(registerEmptyBg)
+    self.registerEmptyImage.snp.makeConstraints { (make) in
+      make.center.equalTo(self.registerEmptyBg)
       make.width.height.equalTo(112)
     }
     
-    reviewLabel.snp.makeConstraints { (make) in
-      make.left.equalTo(registerLabel.snp.left)
-      make.top.equalTo(registerCollectionView.snp.bottom).offset(10)
+    self.reviewLabel.snp.makeConstraints { (make) in
+      make.left.equalTo(self.registerLabel.snp.left)
+      make.top.equalTo(self.registerCollectionView.snp.bottom).offset(10)
     }
     
-    reviewCountLabel.snp.makeConstraints { (make) in
-      make.left.equalTo(reviewLabel.snp.right).offset(5)
-      make.centerY.equalTo(reviewLabel.snp.centerY)
+    self.reviewCountLabel.snp.makeConstraints { (make) in
+      make.left.equalTo(self.reviewLabel.snp.right).offset(5)
+      make.centerY.equalTo(self.reviewLabel.snp.centerY)
     }
     
-    reviewTotalBtn.snp.makeConstraints { (make) in
+    self.reviewTotalButton.snp.makeConstraints { (make) in
       make.right.equalToSuperview().offset(-24)
-      make.centerY.equalTo(reviewLabel.snp.centerY)
+      make.centerY.equalTo(self.reviewLabel)
     }
     
-    reviewTableView.snp.makeConstraints { (make) in
+    self.reviewTableView.snp.makeConstraints { (make) in
       make.left.equalToSuperview().offset(24)
       make.right.equalToSuperview().offset(-24)
       make.bottom.equalToSuperview().offset(130)
-      make.top.equalTo(reviewLabel.snp.bottom).offset(19)
+      make.top.equalTo(self.reviewLabel.snp.bottom).offset(19)
     }
     
-    reviewEmptyImg.snp.makeConstraints { (make) in
+    self.reviewEmptyImage.snp.makeConstraints { (make) in
       make.centerX.equalToSuperview()
-      make.top.equalTo(reviewLabel.snp.bottom).offset(25)
+      make.top.equalTo(self.reviewLabel.snp.bottom).offset(25)
       make.width.height.equalTo(112 * UIScreen.main.bounds.width / 375)
     }
     
-    containerView.snp.makeConstraints { (make) in
+    self.containerView.snp.makeConstraints { (make) in
       make.edges.equalTo(0)
       make.width.equalTo(frame.width)
       make.top.equalToSuperview()
@@ -210,24 +215,28 @@ class MyPageView: BaseView {
     }
   }
   
-  func setRegisterEmpty(isEmpty: Bool, count: Int) {
-    registerEmptyBg.isHidden = !isEmpty
-    registerEmptyImg.isHidden = !isEmpty
+  func bind(user: User) {
+    self.nicknameLabel.text = user.nickname
+  }
+  
+  func setStore(count: Int) {
+    self.registerEmptyBg.isHidden = count != 0
+    self.registerEmptyImage.isHidden = count != 0
     
-    if isEmpty {
-      registerLabel.text = "등록한 음식점이 없어요"
+    if count == 0 {
+      self.registerLabel.text = "my_page_registered_store_empty".localized
     } else {
-      registerLabel.text = "등록한 음식점"
-      registerCountLabel.text = "\(count)개"
+      self.registerLabel.text = "my_page_registered_review".localized
+      self.registerCountLabel.text = "\(count)개"
     }
   }
   
-  func setReviewEmpty(isEmpty: Bool, count: Int) {
-    if isEmpty {
-      reviewLabel.text = "내가 쓴 리뷰가 없어요"
+  func setReview(count: Int) {
+    if count == 0 {
+      self.reviewLabel.text = "my_page_registered_review_empty".localized
     } else {
-      reviewLabel.text = "내가 쓴 리뷰"
-      reviewCountLabel.text = "\(count)개"
+      self.reviewLabel.text = "my_page_registered_review".localized
+      self.reviewCountLabel.text = "\(count)개"
     }
   }
 }
