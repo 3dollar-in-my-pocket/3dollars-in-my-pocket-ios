c404797d929f5498773bb25f587fc33ac332cb12
Yoo Hyun Sik <dbgustlr92@gmail.com>
2021-03-12 13:47:43 +0900

    fix: 애드몹 광고 계정 이전 문제가 있어서 광고 아이디 롤백

--- 3dollar-in-my-pocket/features/category/category-list/CategoryListVC.swift
+++ 3dollar-in-my-pocket/features/category/category-list/CategoryListVC.swift
@@ -146,7 +146,7 @@ class CategoryListVC: BaseVC {
         #if DEBUG
         cell.adBannerView.adUnitID = "ca-app-pub-3940256099942544/2934735716"
         #else
-        cell.adBannerView.adUnitID = "ca-app-pub-5385646520024289/7940737419"
+        cell.adBannerView.adUnitID = "ca-app-pub-1527951560812478/3327283605"
         #endif
     
         cell.adBannerView.rootViewController = self
