46b87258dd2f20c033ed74620e827505aea8a78a
Hyunsik Yoo <dbgustlr92@gmail.com>
2021-09-25 21:33:20 +0900

    feat: M1에서 simulator 빌드하기위해 pod script 추가

---
+++ 3dollar-in-my-pocketTests/stubs/services/StoreServiceMock.swift
@@ -0,0 +1,60 @@
+//
+//  StoreServiceMock.swift
+//  3dollar-in-my-pocketTests
+//
+//  Created by Hyun Sik Yoo on 2021/09/25.
+//  Copyright © 2021 Macgongmon. All rights reserved.
+//
+
+import Foundation
+
+import RxSwift
+
+@testable import dollar_in_my_pocket
+
+struct StoreServiceMock: StoreServiceProtocol {
+  var searchNearStoresObservable: Observable<[StoreInfoResponse]>?
+  
+  func searchNearStores(
+    currentLocation: CLLocation,
+    mapLocation: CLLocation,
+    distance: Double
+  ) -> Observable<[StoreInfoResponse]> {
+    return self.searchNearStoresObservable
+    ?? .error(CommonError(desc: "searchNearStoresObservable가 정의되지 않았습니다."))
+  }
+  
+  func saveStore(addStoreRequest: AddStoreRequest) -> Observable<StoreInfoResponse> {
+    return .empty()
+  }
+  
+  func savePhoto(storeId: Int, photos: [UIImage]) -> Observable<[StoreImageResponse]> {
+    return .empty()
+  }
+  
+  func getPhotos(storeId: Int) -> Observable<[StoreImageResponse]> {
+    return .empty()
+  }
+  
+  func deletePhoto(photoId: Int) -> Observable<String> {
+    return .empty()
+  }
+  
+  func updateStore(storeId: Int, updateStoreRequest: AddStoreRequest) -> Observable<StoreInfoResponse> {
+    return .empty()
+  }
+  
+  func getStoreDetail(storeId: Int, latitude: Double, longitude: Double) -> Observable<StoreDetailResponse> {
+    return .empty()
+  }
+  
+  func getReportedStore(totalCount: Int?, cursor: Int?) -> Observable<Pagination<StoreInfoResponse>> {
+    return .empty()
+  }
+  
+  func deleteStore(storeId: Int, deleteReasonType: DeleteReason) -> Observable<StoreDeleteResponse> {
+    return .empty()
+  }
+  
+  
+}
