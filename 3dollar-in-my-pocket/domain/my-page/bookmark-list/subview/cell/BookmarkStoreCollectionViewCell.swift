81f968428d6b177adbe126d5ec5dc37a60b3571a
Hyunsik Yoo <dbgustlr92@gmail.com>
2022-12-10 14:41:56 +0900

    feat: 즐겨찾기 리스트 가게. 셀 뷰 그리기

---
+++ 3dollar-in-my-pocket/domain/my-page/bookmark-list/subview/cell/BookmarkStoreCollectionViewCell.swift
@@ -0,0 +1,65 @@
+import UIKit
+
+final class BookmarkStoreCollectionViewCell: BaseCollectionViewCell {
+    static let registerId = "\(BookmarkStoreCollectionViewCell.self)"
+    static let size = CGSize(width: UIScreen.main.bounds.width, height: 102)
+    
+    private let containerView = UIView().then {
+        $0.backgroundColor = R.color.gray95()
+        $0.layer.cornerRadius = 15
+    }
+    
+    private let categoryImageView = UIImageView()
+    
+    private let titleLabel = UILabel().then {
+        $0.font = .medium(size: 16)
+        $0.textColor = .white
+        $0.text = "강남역 0번 출구"
+        $0.numberOfLines = 1
+        $0.textAlignment = .left
+    }
+    
+    private let categoriesLabel = UILabel().then {
+        $0.font = .regular(size: 12)
+        $0.textColor = R.color.gray30()
+        $0.text = "#붕어빵 #땅콩과자 #호떡"
+        $0.numberOfLines = 1
+        $0.textAlignment = .left
+    }
+    
+    override func setup() {
+        self.addSubViews([
+            self.containerView,
+            self.categoryImageView,
+            self.titleLabel,
+            self.categoriesLabel
+        ])
+    }
+    
+    override func bindConstraints() {
+        self.containerView.snp.makeConstraints { make in
+            make.left.equalToSuperview().offset(24)
+            make.top.equalToSuperview().offset(6)
+            make.bottom.equalToSuperview().offset(-6)
+            make.right.equalToSuperview().offset(-24)
+        }
+        
+        self.categoryImageView.snp.makeConstraints { make in
+            make.left.equalTo(self.containerView).offset(16)
+            make.top.equalTo(self.containerView).offset(16)
+            make.bottom.equalTo(self.containerView).offset(-16)
+        }
+        
+        self.titleLabel.snp.makeConstraints { make in
+            make.left.equalTo(self.categoryImageView).offset(8)
+            make.top.equalTo(self.categoryImageView).offset(2)
+            make.right.equalTo(self.containerView).offset(-16)
+        }
+        
+        self.categoriesLabel.snp.makeConstraints { make in
+            make.left.equalTo(self.titleLabel)
+            make.top.equalTo(self.titleLabel.snp.bottom).offset(8)
+            make.right.equalTo(self.titleLabel)
+        }
+    }
+}
