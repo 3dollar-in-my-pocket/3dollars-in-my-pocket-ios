057dfc7e56bfaac641ff8ec738d0cd9f817c966e
Yoo Hyun Sik <dbgustlr92@gmail.com>
2022-12-29 14:57:15 +0900

    feat: XCodeCloud 설정 테스트

---
+++ ci_scripts/ci_post_clone.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+#  ci_post_clone.sh
+#  3dollar-in-my-pocket
+#
+#  Created by Hyun Sik Yoo on 2022/12/29.
+#  Copyright © 2022 Macgongmon. All rights reserved.
+
+# Install CocoaPods using Homebrew.
+brew install cocoapods
+brew install git-lfs
+
+git lfs install
+
+# Install dependencies you manage with CocoaPods.
+pod install
