0c00935edb9ece5066bbc7d6f9ee4224f27a46f9
Hyunsik Yoo <dbgustlr92@gmail.com>
2023-02-05 14:14:42 +0900

    #71 NicknameViewModel -> NicknameReactor 리팩토링 완료. 데이터바인딩도 변경점에 맞게 수정

--- 3dollar-in-my-pocket/domain/membership/nickname/NicknameReactor.swift
+++ 3dollar-in-my-pocket/domain/membership/nickname/NicknameReactor.swift
@@ -1,93 +1,135 @@
+import ReactorKit
 import RxSwift
 import RxCocoa
 
-class NicknameViewModel: BaseViewModel {
-  struct Input {
-    let nickname = PublishSubject<String>()
-    let tapStartButton = PublishSubject<Void>()
-  }
-  
-  struct Output {
-    let startButtonEnable = PublishRelay<Bool>()
-    let presentPolicy = PublishRelay<Void>()
-    let errorLabelHidden = PublishRelay<Bool>()
-  }
-  
-  let input = Input()
-  let output = Output()
-  let signinRequest: SigninRequest
-  var userDefaults: UserDefaultsUtil
-  let userService: UserServiceProtocol
+final class NicknameReactor: BaseReactor, Reactor {
+    enum Action {
+        case inputNickname(String)
+        case tapStartButton
+    }
+    
+    enum Mutation {
+        case setNickname(String)
+        case setStartButtonEnable(Bool)
+        case setErrorLabelHidden(Bool)
+        case presentPolicy
+        case showLoading(isShow: Bool)
+        case showErrorAlert(error: Error)
+    }
+    
+    struct State {
+        var nickname: String
+        var isStartButtonEnable: Bool
+        var isErrorLabelHidden: Bool
+        @Pulse var presentPolicy: Void?
+    }
+    
+    let initialState: State
+    private let signinRequest: SigninRequest
+    private var userDefaults: UserDefaultsUtil
+    private let userService: UserServiceProtocol
     var isSignupSuccess = false
-  
-  
-  init(
-    signinRequest: SigninRequest,
-    userDefaults: UserDefaultsUtil,
-    userService: UserServiceProtocol
-  ) {
-    self.signinRequest = signinRequest
-    self.userDefaults = userDefaults
-    self.userService = userService
-    super.init()
     
-    self.input.nickname
-      .map { !$0.trimmingCharacters(in: .whitespaces).isEmpty }
-      .bind(to: self.output.startButtonEnable)
-      .disposed(by: self.disposeBag)
+    init(
+        signinRequest: SigninRequest,
+        userDefaults: UserDefaultsUtil,
+        userService: UserServiceProtocol,
+        state: State = State(
+            nickname: "",
+            isStartButtonEnable: false,
+            isErrorLabelHidden: true
+        )
+    ) {
+        self.signinRequest = signinRequest
+        self.userDefaults = userDefaults
+        self.userService = userService
+        self.initialState = state
+    }
     
-    self.input.tapStartButton
-      .withLatestFrom(self.input.nickname)
-      .bind(onNext: self.setNickname(nickname:))
-      .disposed(by: disposeBag)
-  }
-  
-    private func setNickname(nickname: String) {
+    func mutate(action: Action) -> Observable<Mutation> {
+        switch action {
+        case .inputNickname(let nickname):
+            let isStartButtonEnable = !nickname.trimmingCharacters(in: .whitespaces).isEmpty
+            
+            return .merge([
+                .just(.setNickname(nickname)),
+                .just(.setStartButtonEnable(isStartButtonEnable))
+            ])
+            
+        case .tapStartButton:
+            let nickname = self.currentState.nickname
+            
+            return .concat([
+                .just(.showLoading(isShow: true)),
+                self.setNickname(nickname: nickname),
+                .just(.showLoading(isShow: false))
+            ])
+        }
+    }
+    
+    func reduce(state: State, mutation: Mutation) -> State {
+        var newState = state
+        
+        switch mutation {
+        case .setNickname(let nickname):
+            newState.nickname = nickname
+            
+        case .setStartButtonEnable(let isEnable):
+            newState.isStartButtonEnable = isEnable
+            
+        case .setErrorLabelHidden(let isHidden):
+            newState.isErrorLabelHidden = isHidden
+            
+        case .presentPolicy:
+            newState.presentPolicy = ()
+            
+        case .showLoading(let isShow):
+            self.showLoadingPublisher.accept(isShow)
+            
+        case .showErrorAlert(let error):
+            self.showErrorAlertPublisher.accept(error)
+        }
+        
+        return newState
+    }
+    
+    private func setNickname(nickname: String) -> Observable<Mutation> {
         let signupRequest = SignupRequest(
             name: nickname,
             socialType: self.signinRequest.socialType,
             token: self.signinRequest.token
         )
+        
         if self.isSignupSuccess {
-            self.output.presentPolicy.accept(())
+            return .just(.presentPolicy)
         } else {
-            self.showLoading.accept(true)
-            self.userService.signup(request: signupRequest)
-                .subscribe(
-                    onNext: { [weak self] response in
-                        guard let self = self else { return }
-                        
-                        self.userDefaults.userId = response.userId
-                        self.userDefaults.authToken = response.token
-                        self.isSignupSuccess = true
-                        self.showLoading.accept(false)
-                        self.output.presentPolicy.accept(())
-                    },
-                    onError: self.handleSignupError(error:)
-                )
-                .disposed(by: self.disposeBag)
+            return self.userService.signup(request: signupRequest)
+                .do(onNext: { [weak self] response in
+                    self?.userDefaults.userId = response.userId
+                    self?.userDefaults.authToken = response.token
+                    self?.isSignupSuccess = true
+                })
+                .map { _ in .presentPolicy }
+                .catch { [weak self] error in
+                    return self?.handleSignupError(error: error)
+                    ?? .just(.showErrorAlert(error: BaseError.unknown))
+                }
         }
     }
   
-  private func handleSignupError(error: Error) {
-    self.showLoading.accept(false)
-    if let signupError = error as? SignupError {
-      switch signupError {
-      case .alreadyExistedNickname:
-        self.output.errorLabelHidden.accept(false)
-        
-      case .badRequest:
-        let alertContent = AlertContent(
-          title: nil,
-          message: signupError.errorDescription
-        )
-        
-        self.showSystemAlert.accept(alertContent)
-        self.showLoading.accept(false)
-      }
-    } else {
-      self.showErrorAlert.accept(error)
-      self.showLoading.accept(false)
+    private func handleSignupError(error: Error) -> Observable<Mutation> {
+        if let signupError = error as? SignupError {
+            switch signupError {
+            case .alreadyExistedNickname:
+                return .just(.setErrorLabelHidden(false))
+                
+            case .badRequest:
+                let error = BaseError.custom(signupError.errorDescription ?? "")
+                
+                return .just(.showErrorAlert(error: error))
+            }
+        } else {
+            return .just(.showErrorAlert(error: error))
+        }
     }
-  }
 }
--- 3dollar-in-my-pocket/domain/membership/nickname/NicknameViewController.swift
+++ 3dollar-in-my-pocket/domain/membership/nickname/NicknameViewController.swift
@@ -1,121 +1,127 @@
 import UIKit
+
+import ReactorKit
 import RxSwift
 
-class NicknameViewController: BaseVC {
-  private let nicknameView = NicknameView()
-  private let viewModel: NicknameViewModel
-  
-  
-  init(signinRequest: SigninRequest) {
-    self.viewModel = NicknameViewModel(
-      signinRequest: signinRequest,
-      userDefaults: UserDefaultsUtil(),
-      userService: UserService()
-    )
-    super.init(nibName: nil, bundle: nil)
-  }
-  
-  required init?(coder: NSCoder) {
-    fatalError("init(coder:) has not been implemented")
-  }
-  
-  static func instance(signinRequest: SigninRequest) -> NicknameViewController {
-    return NicknameViewController(signinRequest: signinRequest)
-  }
-  
-  override func loadView() {
-    self.view = self.nicknameView
-  }
-  
-  override func bindViewModel() {
-    // Bind input
-    self.nicknameView.nicknameField.rx.text.orEmpty
-      .bind(to: self.viewModel.input.nickname)
-      .disposed(by: self.disposeBag)
+final class NicknameViewController: BaseViewController, View, NicknameCoordinator {
+    private let nicknameView = NicknameView()
+    private let nicknameReactor: NicknameReactor
+    private weak var coordinator: NicknameCoordinator?
     
-    self.nicknameView.startButton1.rx.tap
-      .throttle(.milliseconds(300), scheduler: MainScheduler.instance)
-      .bind(to: self.viewModel.input.tapStartButton)
-      .disposed(by: self.disposeBag)
-    
-    self.nicknameView.startButton2.rx.tap
-      .throttle(.milliseconds(300), scheduler: MainScheduler.instance)
-      .bind(to: self.viewModel.input.tapStartButton)
-      .disposed(by: self.disposeBag)
-    
-    // Bind output
-    self.viewModel.output.startButtonEnable
-      .asDriver(onErrorJustReturn: false)
-      .drive(self.nicknameView.rx.isStartButtonEnable)
-      .disposed(by: self.disposeBag)
-    
-    self.viewModel.output.presentPolicy
-      .asDriver(onErrorJustReturn: ())
-      .drive(onNext: { [weak self] _ in
-          self?.presentPolicy()
-      })
-      .disposed(by: self.disposeBag)
-    
-    self.viewModel.output.errorLabelHidden
-      .asDriver(onErrorJustReturn: true)
-      .drive(self.nicknameView.rx.isErrorLabelHidden)
-      .disposed(by: self.disposeBag)
-      
-    self.viewModel.showLoading
-      .asDriver(onErrorJustReturn: false)
-      .drive(onNext: { isShow in
-          LoadingManager.shared.showLoading(isShow: isShow)
-      })
-      .disposed(by: self.disposeBag)
-    
-    self.viewModel.showErrorAlert
-      .asDriver(onErrorJustReturn: BaseError.unknown)
-      .drive(onNext: self.showErrorAlert(error:))
-      .disposed(by: self.disposeBag)
-    
-    self.viewModel.showSystemAlert
-      .asDriver(onErrorJustReturn: .init())
-      .drive(onNext: self.showSystemAlert(alert:))
-      .disposed(by: self.disposeBag)
-  }
-  
-  override func bindEvent() {
-    self.nicknameView.backButton.rx.tap
-      .throttle(.milliseconds(300), scheduler: MainScheduler.instance)
-      .asDriver(onErrorJustReturn: ())
-      .drive(onNext: self.popupVC)
-      .disposed(by: self.disposeBag)
-    
-      self.nicknameView.rx.tapBackground
-      .throttle(.milliseconds(300), scheduler: MainScheduler.instance)
-      .asDriver(onErrorJustReturn: ())
-      .drive(onNext: self.nicknameView.hideKeyboard)
-      .disposed(by: self.disposeBag)
-  }
-  
-  override var preferredStatusBarStyle: UIStatusBarStyle {
-    return .lightContent
-  }
-  
-  private func popupVC() {
-    self.navigationController?.popViewController(animated: true)
-  }
-  
-  private func goToMain() {
-    if let sceneDelegate = UIApplication.shared.connectedScenes.first?.delegate as? SceneDelegate {
-      sceneDelegate.goToMain()
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent
     }
-  }
     
-    private func presentPolicy() {
-        let viewController = PolicyViewController.instance(delegate: self)
+    static func instance(signinRequest: SigninRequest) -> NicknameViewController {
+        return NicknameViewController(signinRequest: signinRequest)
+    }
+  
+    init(signinRequest: SigninRequest) {
+        self.nicknameReactor = NicknameReactor(
+            signinRequest: signinRequest,
+            userDefaults: UserDefaultsUtil(),
+            userService: UserService()
+        )
         
-        self.present(viewController, animated: true)
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func loadView() {
+        self.view = self.nicknameView
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.coordinator = self
+        self.reactor = self.nicknameReactor
+    }
+    
+    override func bindEvent() {
+        self.nicknameView.backButton.rx.tap
+            .throttle(.milliseconds(300), scheduler: MainScheduler.instance)
+            .asDriver(onErrorJustReturn: ())
+            .drive(onNext: { [weak self] _ in
+                self?.coordinator?.presenter
+                    .navigationController?
+                    .popViewController(animated: true)
+            })
+            .disposed(by: self.eventDisposeBag)
+        
+        self.nicknameView.rx.tapBackground
+            .throttle(.milliseconds(300), scheduler: MainScheduler.instance)
+            .asDriver(onErrorJustReturn: ())
+            .drive(onNext: { [weak self] _ in
+                self?.nicknameView.hideKeyboard()
+            })
+            .disposed(by: self.eventDisposeBag)
+        
+        self.nicknameReactor.showLoadingPublisher
+            .asDriver(onErrorJustReturn: false)
+            .drive(onNext: { [weak self] isShow in
+                self?.coordinator?.showLoading(isShow: isShow)
+            })
+            .disposed(by: self.eventDisposeBag)
+        
+        self.nicknameReactor.showErrorAlertPublisher
+            .asDriver(onErrorJustReturn: BaseError.unknown)
+            .drive(onNext: { [weak self] error in
+                self?.coordinator?.showErrorAlert(error: error)
+            })
+            .disposed(by: self.eventDisposeBag)
+    }
+    
+    
+    func bind(reactor: NicknameReactor) {
+        // Bind Action
+        self.nicknameView.nicknameField.rx.text.orEmpty
+            .map { Reactor.Action.inputNickname($0) }
+            .bind(to: reactor.action)
+            .disposed(by: self.disposeBag)
+        
+        self.nicknameView.startButton1.rx.tap
+            .throttle(.milliseconds(300), scheduler: MainScheduler.instance)
+            .map { Reactor.Action.tapStartButton }
+            .bind(to: reactor.action)
+            .disposed(by: self.disposeBag)
+        
+        self.nicknameView.startButton2.rx.tap
+            .throttle(.milliseconds(300), scheduler: MainScheduler.instance)
+            .map { Reactor.Action.tapStartButton }
+            .bind(to: reactor.action)
+            .disposed(by: self.disposeBag)
+        
+        // Bind State
+        reactor.state
+            .map { $0.isStartButtonEnable }
+            .distinctUntilChanged()
+            .asDriver(onErrorJustReturn: false)
+            .drive(self.nicknameView.rx.isStartButtonEnable)
+            .disposed(by: self.disposeBag)
+        
+        reactor.state
+            .map { $0.isErrorLabelHidden }
+            .distinctUntilChanged()
+            .asDriver(onErrorJustReturn: true)
+            .drive(self.nicknameView.rx.isErrorLabelHidden)
+            .disposed(by: self.disposeBag)
+        
+        reactor.pulse(\.$presentPolicy)
+            .compactMap { $0 }
+            .asDriver(onErrorJustReturn: ())
+            .drive(onNext: { [weak self] _ in
+                self?.coordinator?.presentPolicy()
+            })
+            .disposed(by: self.disposeBag)
     }
 }
 
 extension NicknameViewController: PolicyViewControllerDelegate {
     func onDismiss() {
-        self.goToMain()
+        self.coordinator?.goToMain()
     }
 }
