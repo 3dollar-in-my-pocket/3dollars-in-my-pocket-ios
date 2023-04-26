45c21644abfadc4443772c88572c98dcbe6af570
Yoo Hyun Sik <dbgustlr92@gmail.com>
2023-04-26 22:25:15 +0900

    fix: 코드 스멜 제거

--- 3dollar-in-my-pocket/manager/deeplink/DeeplinkManager.swift
+++ 3dollar-in-my-pocket/manager/deeplink/DeeplinkManager.swift
@@ -139,7 +139,7 @@ final class DeeplinkManager: DeeplinkManagerProtocol {
                     targetViewController,
                     animated: true
                 )
-            } else if let _ = rootViewController as? SplashVC {
+            } else if rootViewController is SplashVC {
                 self.reserveDeeplink(deeplinkContents: contents)
             } else {
                 Log.error("UINavigationViewController가 없습니다.")
