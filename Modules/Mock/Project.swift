4a7956dfbf45506f54a6c7b29b30ccf8d8003d8f
Hyunsik Yoo <dbgustlr92@gmail.com>
2023-12-17 20:52:23 +0900

    tuist 코드 일부 정리

--- Modules/Mock/Project.swift
+++ Modules/Mock/Project.swift
@@ -1,46 +1,11 @@
 import ProjectDescription
+import ProjectDescriptionHelpers
 
-struct Version {
-    static let targetVersion = "14.0"
-}
-
-struct BuildSetting {
-    struct Project {
-        static let base: SettingsDictionary = [
-            "IPHONEOS_DEPLOYMENT_TARGET": "14.0"
-        ]
-    }
-}
-
-let project = Project(
+let project = ModuleProvider.makeModule(
     name: "Mock",
-    organizationName: "macgongmon",
-    packages: [],
-    settings: .settings(
-        base: BuildSetting.Project.base,
-        configurations: [
-            .debug(name: .debug),
-            .release(name: .release)
-        ]
-    ),
-    targets: [
-        Target(
-            name: "Mock",
-            platform: .iOS,
-            product: .staticLibrary,
-            bundleId: "com.macgongmon.-dollar-in-my-pocket.mock",
-            deploymentTarget: .iOS(targetVersion: Version.targetVersion, devices: .iphone),
-            sources: ["Sources/**"],
-            dependencies: [
-                .project(target: "AppInterface", path: "../../App"),
-                .project(target: "DependencyInjection", path: "../DependencyInjection"),
-            ]
-        )
-    ],
-    schemes: [
-        Scheme(
-            name: "Mock",
-            buildAction: BuildAction(targets: ["Mock"])
-        )
+    product: .staticLibrary,
+    dependencise: [
+        .appInterface,
+        .dependencyInjection
     ]
 )
---
+++ Tuist/ProjectDescriptionHelpers/DefaultSetting.swift
@@ -0,0 +1,36 @@
+import ProjectDescription
+
+public struct DefaultSetting {
+    public static let targetVersion: SettingValue = "14.0"
+    public static let appVersion: SettingValue = "4.0.3"
+    public static let buildNumber: SettingValue = "1"
+    public static let organizaationName = "macgongmon"
+    public static let appIdentifier = "-dollar-in-my-pocket"
+    
+    public static let baseProductSetting: SettingsDictionary = [
+        "IPHONEOS_DEPLOYMENT_TARGET": targetVersion
+    ]
+}
+
+public extension DefaultSetting {
+    static func bundleId(moduleName: String) -> String {
+        return "com.\(organizaationName).\(appIdentifier).\(moduleName)"
+    }
+}
+
+
+public extension SettingValue {
+    var stringValue: String {
+        switch self {
+        case .string(let string):
+            return string
+            
+        case .array(let array):
+            guard let value = array.first else { return "" }
+            return value
+            
+        @unknown default:
+            return ""
+        }
+    }
+}
