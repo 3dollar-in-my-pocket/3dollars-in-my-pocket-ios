98179ec14eca25f786f8ad1051163fbdaf1830c1
Hakyung Kim <hkhk4862@gmail.com>
2024-04-10 20:35:12 +0900

    TH-119 : 최근 검색어 Cell 추가

--- Modules/Feature/Home/Targets/Home/Sources/Domains/SearchAddress/RecentSearchAddressCellViewModel.swift
+++ Modules/Feature/Home/Targets/Home/Sources/Domains/SearchAddress/RecentSearchAddressCellViewModel.swift
@@ -4,6 +4,49 @@ import Common
 import Model
 
 final class RecentSearchAddressCellViewModel: BaseViewModel {
+    struct Input {
+        let didTapDeleteButton = PassthroughSubject<Void, Never>()
+    }
+
+    struct Output {
+        let placeName: String
+        let addressName: String?
+        let deleteRecentSearch = PassthroughSubject<String, Never>()
+    }
+
+    let input = Input()
+    let output: Output
+    
+    private let data: PlaceResponse
+
+    init(data: PlaceResponse) {
+        self.data = data
+        
+        let addressName: String? = {
+            return if let roadAddressName = data.roadAddressName, roadAddressName.isNotEmpty {
+                roadAddressName
+            } else {
+                data.addressName
+            }
+        }()
+        
+        self.output = Output(
+            placeName: data.placeName,
+            addressName: addressName
+        )
+        super.init()
+    }
+
+    override func bind() {
+        super.bind()
+
+        input.didTapDeleteButton
+            .withUnretained(self)
+            .sink { owner, _ in
+                owner.output.deleteRecentSearch.send(owner.data.placeId)
+            }
+            .store(in: &cancellables)
+    }
 }
 
 extension RecentSearchAddressCellViewModel: Identifiable {
