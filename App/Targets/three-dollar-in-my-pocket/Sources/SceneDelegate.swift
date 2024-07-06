19b0888268e1e8a20cad9b26c2f90a0bd90870f7
Hyunsik Yoo <dbgustlr92@gmail.com>
2024-07-06 17:16:39 +0900

    TH-239: netfox 적용

--- App/Targets/three-dollar-in-my-pocket/Sources/SceneDelegate.swift
+++ App/Targets/three-dollar-in-my-pocket/Sources/SceneDelegate.swift
@@ -5,6 +5,7 @@ import Membership
 
 import KakaoSDKAuth
 import FirebaseDynamicLinks
+import netfox
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     var window: UIWindow?
@@ -20,7 +21,12 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     ) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         
+        #if DEBUG
+        window = ShakeDetectingWindow(frame: windowScene.coordinateSpace.bounds)
+        #else
         window = UIWindow(frame: windowScene.coordinateSpace.bounds)
+        #endif
+        
         window?.windowScene = windowScene
         window?.backgroundColor = UIColor.init(r: 28, g: 28, b: 28)
         window?.rootViewController = SplashViewController(nibName: nil, bundle: nil)
@@ -100,5 +106,19 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let userInfo = connectionOptions.notificationResponse?.notification.request.content.userInfo,
               let deeplink = userInfo["link"] as? String else { return }
         DeeplinkManager.shared.reserveDeeplink(url: URL(string: deeplink))
+    }
+}
+
+extension SceneDelegate {
+    final class ShakeDetectingWindow: UIWindow {
+        override func motionEnded(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
+            if motion == .motionShake {
+                let notificationFeedbackGenerator = UINotificationFeedbackGenerator()
+                notificationFeedbackGenerator.prepare()
+                notificationFeedbackGenerator.notificationOccurred(.success)
+                NFX.sharedInstance().show()
+            }
+            super.motionEnded(motion, with: event)
+        }
     }
 }
--- Tuist/ProjectDescriptionHelpers/Package+.swift
+++ Tuist/ProjectDescriptionHelpers/Package+.swift
@@ -10,4 +10,6 @@ public extension Package {
     static let permissionsKit = Package.remote(url: "https://github.com/sparrowcode/PermissionsKit", requirement: .exact("9.0.2"))
     
     static let firebaseSDK = Package.remote(url: "https://github.com/firebase/firebase-ios-sdk", requirement: .exact("10.22.1"))
+    
+    static let netfox = Package.remote(url: "https://github.com/kasketis/netfox", requirement: .exact("1.21.0"))
 }
