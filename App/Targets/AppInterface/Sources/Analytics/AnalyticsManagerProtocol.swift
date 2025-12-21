f93d011352bcf2fa3d3e5b7f638aa88a049cb3e8
Hyunsik Yoo <dbgustlr92@gmail.com>
2025-12-21 14:44:30 +0900

    TH-964 : 사용하지 않는 매니저 제거

--- App/Targets/AppInterface/Sources/Analytics/AnalyticsManagerProtocol.swift
+++
@@ -1,9 +0,0 @@
-public protocol AnalyticsManagerProtocol {
-    func logPageView(screen: AnalyticsScreen, type: AnyObject.Type)
-    
-    func logEvent(event: AnalyticsEvent, screen: AnalyticsScreen)
-    
-    func setPushEnable(isEnable: Bool)
-    
-    func setUser(id: String?)
-}
